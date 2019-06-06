56b4f228e670c9883f581b810d7b9eb336949d1d
Chris McKay <mckay.chris@codeimmersives.com>
2019-06-06 11:55:33 -0400

    adjusted conn

--- conn/connBlogs.php
+++ conn/connBlogs.php
@@ -5,10 +5,6 @@ $db_pass = 'mysql';
 $db_name = 'blogging';
 $db_host = 'localhost';
 
-// your actual bluehost credentials
-$db_user2 = 'ezshocom_Brian';
-$db_pass2 = 'Abc123$1'; 
-$db_name2 = 'ezshocom_mywebsite';
 
 
 // local ampps
@@ -17,8 +13,4 @@ $conn = mysqli_connect($db_host, $db_user, $db_pass) or die('Could not connect t
 mysqli_select_db($conn, $db_name) or die('Could not select the database');
 
 
-// remote bluehost
-//$conn = mysqli_connect($db_host, $db_user2, $db_pass2) or die('Could not connect to the database server');
-//
-//mysqli_select_db($conn, $db_name2) or die('Could not select the database');
 
